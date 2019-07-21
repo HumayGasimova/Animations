0524f15c6a242d84bf45365f0a101c43477edbf9
Humay <crypto347@gmail.com>
2019-07-21 12:57:18 +0400

    ImagesPage component added

---
+++ src/components/Parallax/ImagePage/imagesPage.js
@@ -0,0 +1,105 @@
+/**
+ * Libraries
+ */
+
+import React,{
+    Component
+ } from 'react';
+ 
+ /**
+  * Components
+  */
+ import ImageSlot from './ImageSlot/imageSlot';
+ import Pic1 from '../../../images/SlotImages/IMG_20170226_150338.jpg'
+ import Pic2 from '../../../images/SlotImages/IMG_20170404_133938.jpg'
+ import Pic3 from '../../../images/SlotImages/IMG_20180515_081708_977.jpg'
+ import Pic4 from '../../../images/SlotImages/IMG_20180707_123405_284.jpg'
+ import Pic5 from '../../../images/SlotImages/IMG_20180725_165300_509.jpg'
+ import Pic6 from '../../../images/SlotImages/IMG_20180731_155539_567.jpg'
+ import Pic7 from '../../../images/SlotImages/IMG_20180808_170700_758.jpg'
+ import Pic8 from '../../../images/SlotImages/IMG_20180814_174126_452.jpg'
+ import Pic9 from '../../../images/SlotImages/IMG_20181001_143645_993.jpg'
+
+ 
+ /**
+  * Styles
+  */
+import './imagesPage.scss';
+ 
+ /**
+  * App component definition and export
+  */
+ export class ImagesPage extends Component {
+ 
+    /**
+      * Constructor
+      */
+ 
+     constructor(props) {
+        super(props);
+        this.state = {
+           imageSlots: [Pic1,Pic2,Pic3,Pic4,Pic5,Pic6,Pic7,Pic8,Pic9]
+        }
+    }
+  
+//    componentDidMount = () => {
+//     window.addEventListener('scroll', this.handleScroll)
+//  }
+
+//     handleScroll = () => {
+//         let scrollHeight = document.body.scrollTop
+//          this.setState({
+//              slower: scrollHeight/2,
+//              slower2x: scrollHeight/10
+//          })
+//     }
+
+   //  renderImage = () => {
+   //      if(this.props.left){
+   //         return(
+   //              <img 
+   //                  src={AirBalloonLogo}
+   //                  style={{transform: `translate(0px, -${this.state.slower}px)`}}
+   //              />
+   //         )}else{
+   //          return(
+   //              <img 
+   //              src={AirBalloonLogo}
+   //              style={{transform: `translate(0px, -${this.state.slower2x}px)`}}
+   //          />
+   //         ) }
+        
+   //  }
+    
+    renderImages = () => {
+        return(
+           <div className="slots">
+              {this.state.imageSlots.map((el,i)=>{
+                 return (
+                       <ImageSlot
+                          key={i}
+                          // onClick={() => {this.handleOnClick(i)}}
+                          // clicked={this.state.clicked}
+                          // number={"number" + i}
+                          image={el}
+                       >
+                          {"Humay"}
+                       </ImageSlot>
+                 )
+              })}    
+           </div>
+        )
+      }
+ 
+
+    render(){
+       return(
+            <div>
+                {this.renderImages()}
+            </div>
+       );
+    }
+ }
+ 
+ export default ImagesPage;
+ 
--- src/components/Parallax/Parallax.js
+++ src/components/Parallax/Parallax.js
@@ -13,16 +13,7 @@ import HeaderText from './Text/HeaderText/headerText';
 import Stars from './Stars/stars';
 import Unicorn from './Unicorn/unicorn';
 import AirBalloon from './AirBalloon/airBalloon';
-import ImageSlot from './ImageSlot/imageSlot';
-import Pic1 from '../../images/SlotImages/IMG_20170226_150338.jpg'
-import Pic2 from '../../images/SlotImages/IMG_20170404_133938.jpg'
-import Pic3 from '../../images/SlotImages/IMG_20180515_081708_977.jpg'
-import Pic4 from '../../images/SlotImages/IMG_20180707_123405_284.jpg'
-import Pic5 from '../../images/SlotImages/IMG_20180725_165300_509.jpg'
-import Pic6 from '../../images/SlotImages/IMG_20180731_155539_567.jpg'
-import Pic7 from '../../images/SlotImages/IMG_20180808_170700_758.jpg'
-import Pic8 from '../../images/SlotImages/IMG_20180814_174126_452.jpg'
-import Pic9 from '../../images/SlotImages/IMG_20181001_143645_993.jpg'
+import ImagesPage from './ImagePage/imagesPage';
 
 /**
  * Styles
@@ -43,31 +34,8 @@ export class Parallax extends Component {
 
    constructor(props) {
       super(props);
-      this.state = {
-         imageSlots: [Pic1,Pic2,Pic3,Pic4,Pic5,Pic6,Pic7,Pic8,Pic9]
-      }
-}
-
-renderImages = () => {
-   return(
-      <div className="slots">
-         {this.state.imageSlots.map((el,i)=>{
-            return (
-                  <ImageSlot
-                     key={i}
-                     // onClick={() => {this.handleOnClick(i)}}
-                     // clicked={this.state.clicked}
-                     // number={"number" + i}
-                     image={el}
-                  >
-                     {"Humay"}
-                  </ImageSlot>
-            )
-         })}    
-      </div>
-   )
- }
-
+      
+   }
 
    render(){
       return(
@@ -98,7 +66,7 @@ renderImages = () => {
                   <Unicorn />
                </div>
             </div>
-               {this.renderImages()}
+               <ImagesPage/>
          </div>
       );
    }
