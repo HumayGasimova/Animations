b02a275ff4d38998627a452d1dc98c97f96cebfa
Humay <crypto347@gmail.com>
2019-08-11 13:55:49 +0400

    CssTransition Component(instead of Transition Component)

--- src/components/AnimationCss/DialogBox/dialogBox.js
+++ src/components/AnimationCss/DialogBox/dialogBox.js
@@ -7,6 +7,8 @@ import React,{
 } from 'react';
 
 import Transition from 'react-transition-group/Transition';
+import CSSTransition from 'react-transition-group/CSSTransition';
+
 /**
  * Components
  */
@@ -34,28 +36,43 @@ export class DialogBox extends Component {
 
    render(){
       return(
-      <Transition 
-         in={this.props.dialogBoxIsOpen} 
-         timeout={animationTiming}
-         mountOnEnter
-         unmountOnExit
-         onEnter={()=>console.log("onEnter")}
-         onEntering={()=>console.log("onEntering")}
-         onEntered={()=>console.log("onEntered")}
-         onExit={()=>console.log("onExit")}
-         onExiting={()=>console.log("onExiting")}
-         onExited={()=>console.log("onExited")}
-      > 
-         {state => (
-            <div 
-               // className={this.props.dialogBoxIsOpen ? "dialogBox open" : "dialogBox close"}
-               className={state === "entering" ? "dialogBox open" : state === "exiting" ? "dialogBox close": "dialogBox"}
-               >
-                  <h1>Dialog Box</h1>
-                  <button onClick={this.props.handleDialogBoxClose}>Close Dialog Box</button>
-            </div>
-         )}
-      </Transition>
+      // <Transition 
+      //    in={this.props.dialogBoxIsOpen} 
+      //    timeout={animationTiming}
+      //    mountOnEnter
+      //    unmountOnExit
+      //    onEnter={()=>console.log("onEnter")}
+      //    onEntering={()=>console.log("onEntering")}
+      //    onEntered={()=>console.log("onEntered")}
+      //    onExit={()=>console.log("onExit")}
+      //    onExiting={()=>console.log("onExiting")}
+      //    onExited={()=>console.log("onExited")}
+      // > 
+      //    {state => (
+      //       <div 
+      //          // className={this.props.dialogBoxIsOpen ? "dialogBox open" : "dialogBox close"}
+      //          className={state === "entering" ? "dialogBox open" : state === "exiting" ? "dialogBox close": "dialogBox"}
+      //          >
+      //             <h1>Dialog Box</h1>
+      //             <button onClick={this.props.handleDialogBoxClose}>Close Dialog Box</button>
+      //       </div>
+      //    )}
+      // </Transition>
+      <CSSTransition 
+      in={this.props.dialogBoxIsOpen} 
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames="fade-slide"
+   > 
+         <div 
+            // className={this.props.dialogBoxIsOpen ? "dialogBox open" : "dialogBox close"}
+            className={"dialogBox"}
+            >
+               <h1>Dialog Box</h1>
+               <button onClick={this.props.handleDialogBoxClose}>Close Dialog Box</button>
+         </div>
+   </CSSTransition>
       
       );
    }
